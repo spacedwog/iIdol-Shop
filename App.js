0947cb5ebc655ab3b678ffaeba0c49866e48bb62
Spacedwog <owner@spacedwog.com.br>
2024-10-13 14:49:51 -0300

    Commited

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return ( < View style = { styles.container } >
+        <
+        Text > Seja Bem - Vindo à iIdol Shop < /Text>   <
+        StatusBar style = "auto" / >
+        <
+        /View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });

b69bcddbc80bb4b1567b49174bb394fc9975b9d5
Spacedwog <owner@spacedwog.com.br>
2024-10-13 15:17:17 -0300

    Commited

--- App.js
+++ App.js
@@ -1,22 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
+import { StatusBar, SafeAreaView, View } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
+import Projetos from './src/telas/Projetos';
 
 export default function App() {
-    return ( < View style = { styles.container } >
+    return ( <
+        View >
         <
-        Text > Seja Bem - Vindo à iIdol Shop < /Text>   <
+        Projetos / >
+        <
         StatusBar style = "auto" / >
         <
         /View>
     );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-});
---
+++ src/telas/Projetos.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+export default function Projetos() {
+    return <
+        Text > Projetos < /Text>
+}
